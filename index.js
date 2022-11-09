a9da5dbaabd6528d53bddafd4610d2abd06d0735
Ilya <ilya.shuvatov1@gmail.com>
2022-11-09 07:03:56 +0100

    task2

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const reasonText = 'Мне это нравится и я хочу работат
 const numberOfMonth = '3'
 
 console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${proframmingLanguage} на курсе по ${proframmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал ${proframmingLanguage}, ${numberOfMonth} месяца. Я уверен что пройду данный курс до конца`)
+
+let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${proframmingLanguage} на курсе по ${proframmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал ${proframmingLanguage}, ${numberOfMonth} месяца. Я уверен что пройду данный курс до конца`
+
+myInfoText = myInfoText.replaceAll(proframmingLanguage, 'javascript')
+console.log(myInfoText)
+
+myInfoText = myInfoText.replaceAll('курс', 'КУРС')
+console.log(myInfoText)
+
+console.log(myInfoText[0])
+console.log(myInfoText[myInfoText.length - 1])
