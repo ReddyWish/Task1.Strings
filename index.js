d6c5252682fc3f9b85da8ecd2b2613c1d73868ad
Ilya <ilya.shuvatov1@gmail.com>
2022-11-09 06:53:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const myName = 'Илья';
+const proframmingLanguage = 'JavaScript';
+const courseCreatorName = 'Владилен Минин';
+const reasonText = 'Мне это нравится и я хочу работать в этой сфере';
+const numberOfMonth = '3'
+
+console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${proframmingLanguage} на курсе по ${proframmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал ${proframmingLanguage}, ${numberOfMonth} месяца. Я уверен что пройду данный курс до конца`)
